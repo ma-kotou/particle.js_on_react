7e2bfc9f7c22cf1b2f2966b67dd8983da0432fec
kotou@actynetwork <maikotou@MainoiMac.local>
2023-03-06 11:43:10 +0900

    first-commit

---
+++ src/pages/index.js
@@ -0,0 +1,133 @@
+import * as React from "react"
+import Particle from "react-tsparticles";
+
+const pageStyles = {
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  backgroundColor: "#b61924",
+  height: "100vh"
+}
+
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <Particle options={{
+        "particles": {
+    "number": {
+      "value": 9,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 1,
+        "color": "#ffffff",
+        "opacity": "0.6"
+      },
+      "polygon": {
+        "nb_sides": 10
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 0,
+        "size_min": 40,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 50,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "hover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 60,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true      }}/>
+
+
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
